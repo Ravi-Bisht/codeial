9ceb95fdb03e778a030436d154c03b14c64ee932
Ravi-Bisht <ravibisht414@gmail.com>
2023-02-22 18:05:46 +0530

    Sass & likes andn posts

--- controllers/user_controller.js
+++ controllers/user_controller.js
@@ -17,7 +17,12 @@ module.exports.profile = function(req,res){
 
 //render the sign up page
 module.exports.signUp = function(req,res){
-    return res.render("User_sign_up",{
+
+    if(req.isAuthenticated()){
+        return res.redirect('/users/profile');
+    }
+
+    return res.render("user_sign_up",{
         title : "Codeial | Sign Up"
     })
 }
@@ -25,7 +30,11 @@ module.exports.signUp = function(req,res){
 
 // render the sign in page 
 module.exports.signIn = function(req,res){
-    return res.render("User_sign_in",{
+
+    if(req.isAuthenticated()){
+        return res.redirect('/users/profile');
+    }
+    return res.render("user_sign_in",{
         title : "Codeial | Sign In"
     })
 }
@@ -55,4 +64,24 @@ module.exports.create = function(req,res){
 // sign in and create the session for the user 
 module.exports.createSession = function(req,res){
     //todo later
+
+    return res.redirect('/')
 }
+
+
+
+// sign out
+
+module.exports.destroySession = function(req,res){
+//      req.logout();
+
+//     return res.redirect('/');
+
+// new syntax 
+
+req.logout(function(err) {
+    if (err) { return next(err); }
+    res.redirect('/');
+  });
+}
+
